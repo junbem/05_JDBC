af599a38a1306aaf4b91d0264f9d041d25e8423d
jungho <kiik52@naver.com>
2025-04-18 16:46:50 +0900

    chap01-connection - 오류 수정 con = DriverManager.getConnection(url, props); 추가

--- 20_JIJUNGHO/chap01_connection-lecture-source/src/main/java/com/ohgiraffers/section02/template/JDBCTemplate.java
+++ 20_JIJUNGHO/chap01_connection-lecture-source/src/main/java/com/ohgiraffers/section02/template/JDBCTemplate.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section02.template;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -18,6 +19,7 @@ public class JDBCTemplate {
             props.load(new FileReader("src/main/java/com/ohgiraffers/section01/connection/jdbc-config.properties"));
 
             String url = props.getProperty("url");
+            con = DriverManager.getConnection(url, props);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
